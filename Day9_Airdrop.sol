1701dfe9832f064f70cd01399ebd654132481d3f
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-06-23 16:41:26 +0300

    Update Day9_Airdrop.sol

--- Day9_Airdrop.sol
+++ Day9_Airdrop.sol
@@ -6,16 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Airdrop is Ownable {
     ERC20 private token;
-    mapping(address => uint256) private claim;
+    mapping(address => bool) private claim;
     uint amount = 100000000000000000000;
 
     constructor(address _tokenAddr) {
         token = ERC20(_tokenAddr);
     }
-    
+
     //function where every user can claim tokens
     function getAirdrop() public {
-        require(token.balanceOf(this) >= amount, "Balance not enough.");
+        require(token.balanceOf(address(this)) >= amount, "Balance not enough.");
         require(claim[msg.sender] == false, "Already claimed.");
         token.transfer(msg.sender, amount); //18 decimals token
         claim[msg.sender] = true;
@@ -23,7 +23,7 @@ contract Airdrop is Ownable {
 
     //function to give airdrop to a set of addresses
     function payAirdrop(address[] memory _recipients, uint _amount) onlyOwner public {
-        uint tot_amount = token.balanceOf(this);
+        uint tot_amount = token.balanceOf(address(this));
         require(tot_amount > _amount*_recipients.length, "Not enough balance");
         for (uint256 i = 0; i < _recipients.length; ++i) {
             token.transfer(msg.sender, _amount);
@@ -31,4 +31,3 @@ contract Airdrop is Ownable {
     }
 
 }
-
