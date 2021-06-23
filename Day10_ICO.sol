aed5731d697d8efe75b0f2c82d79bc4f0bfa5457
Lorenzo Primiterra <lorenzoprimiterra@gmail.com>
2021-06-23 16:48:42 +0300

    Create Day10_ICO.sol

---
+++ Day10_ICO.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ICO is Ownable {
+    ERC20 private token;
+    uint startDate;
+    uint price;
+    
+    constructor(address _tokenAddr, uint _startDate, uint _price) {
+        startDate = _startDate;
+        price = _price;
+        token = ERC20(_tokenAddr);
+    }
+
+    modifier saleStarted() {
+        require(block.timestamp >= startDate, "Sale hasn't started yet.");
+        _;
+    }
+
+    //Very simple function. Need to add all the ERC20 approve and send logic
+    function buyTokens() payable external saleStarted {
+        uint tot_amount = token.balanceOf(address(this));
+        uint amount = price * msg.value;
+        require(tot_amount > amount, "Not enough balance");
+        token.transfer(msg.sender, amount);
+    }
+
+}
+
